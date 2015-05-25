7df8f1405b50b15eb5453adf1c806787ab7351a6
rb643 <rb643@cam.ac.uk>
2015-05-25 19:06:59 +0100

    Update restingstate.m

--- restingstate.m
+++ restingstate.m
@@ -11,6 +11,7 @@ cfg.trialdef.prestim = -5; %don't take the whole one minute
 cfg.trialdef.poststim = 55;
 cfg.viewmode = 'vertical';
 cfg.ylim ='maxmin';
+
 cfg = ft_definetrial(cfg);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -23,7 +24,7 @@ cfg.lpfreq = 60;
 cfg.reref = 'yes';
 cfg.refchannel = 'all';
 cfg.bsfilter = 'yes';
-%cfg.bsfreq = [49 51]; %filter out 50Hz
+cfg.bsfreq = [49 51]; %filter out 50Hz
 cleandata = ft_preprocessing(cfg);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -46,7 +47,7 @@ cfg_int.badchannel           = 'PO8';
 cfg_int.layout               = 'biosemi64.lay';
 cfg_int.method               = 'nearest';
 cfg_int.neighbours           = neighbours;
-cfg_int.neighbourdist        = 0.13; %not too sure about this?
+cfg_int.neighbourdist        = 0.13;
 artifact_cleandata           = ft_channelrepair(cfg_int,cleandata)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -60,14 +61,22 @@ artifact_cleandata = rmfield(artifact_cleandata,'trialinfo');
 % cut it into little pieces
 epocheddata = ft_redefinetrial(cfg_cut, artifact_cleandata);
 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % OPTIONAL visual inspection
+% cfg_vis = [];
+% cfg_vis.method = 'trial';
+% cfg_vis.alim = 5e-5;
+% dummy = ft_rejectvisual(cfg_vis,epocheddata);
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% ICA denoising settings
 cfg_ica = [];
 cfg_ica.method  = 'runica';
-cfg_ica.channel = 1:64; %EEG channels only
+cfg_ica.channel = 1:63; %EEG channels only, minus the number of repaired channels (e.g. the repaired channel can not be independent)
 datacomp = ft_componentanalysis(cfg_ica, epocheddata);
 
 %%  plot components
+cfg_icapl = [];
 cfg_icapl.channel = [1:15]; %components to be plotted
 cfg_icapl.viewmode = 'component';
 cfg_icapl.layout = 'biosemi64.lay';
@@ -79,8 +88,8 @@ ft_databrowser(cfg_icapl, datacomp)
 %components = inputdlg('Enter components to be removed:','Components');
 %components = str2num(cell2mat(components));
 %cfg_ica.component = [component]
-cfg_ica.component = [35 64]; %exact numbers will vary per individual
-data_iccleaned = ft_rejectcomponent(cfg_ica, datacomp);
+cfg_ica.component = []; %exact numbers will vary per individual
+data_iccleaned = ft_rejectcomponent(cfg_ica, datacomp, epocheddata); %clean and backpropagate onto original 64-channel data
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Do some clean-up
@@ -93,7 +102,8 @@ cfg_freq.method = 'wavelet';
 cfg_freq.output = 'powandcsd';
 cfg_freq.channel = 1:64;
 cfg_freq.keeptrials ='yes'; %do not return an average of all trials for subsequent wpli analysis
-cfg_freq.toi = [0:0.05:4];
+cfg_freq.toi = [0.5:0.05:3.5];
+%cfg_freq.foi = [0.5:0.02:4];
 cfg_freq.foi = [0.5:0.02:4];
 [freq_data.delta] = ft_freqanalysis(cfg_freq, data_iccleaned);
 cfg_freq.foi = [4:0.02:7];
@@ -104,13 +114,3 @@ cfg_freq.foi = [13:0.02:30];
 [freq_data.beta] = ft_freqanalysis(cfg_freq, data_iccleaned);
 cfg_freq.foi = [30:0.02:60];
 [freq_data.gamma] = ft_freqanalysis(cfg_freq, data_iccleaned);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% connectivity analysis
-cfg_conn = [];
-cfg_conn.method = 'wpli';
-stat.delta = ft_connectivityanalysis(cfg_conn, freq_data.delta); 
-stat.theta = ft_connectivityanalysis(cfg_conn, freq_data.theta); 
-stat.alpha = ft_connectivityanalysis(cfg_conn, freq_data.alpha); 
-stat.beta = ft_connectivityanalysis(cfg_conn, freq_data.beta); 
-stat.gamma = ft_connectivityanalysis(cfg_conn, freq_data.gamma); 
